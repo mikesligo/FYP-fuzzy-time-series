0885d1a40652fac9c71ddbef3f6c72dcadd9122e
Mike Gallagher <mikesligo@gmail.com>
2014-03-16 19:04:01 +0000

    calculated fuzzy sets

--- fuzzy/fuzzy_set_builder.py
+++ fuzzy/fuzzy_set_builder.py
@@ -7,18 +7,39 @@ class Fuzzy_set_builder(object):
         self.__intervals = intervals
 
     def calculate_fuzzy_sets(self):
+        if len(self.__intervals) < 3:
+            print "Not enough intervals for calculating fuzzy sets"
+
         fuzzy_sets = []
+        for set_idx, set_counter in enumerate(self.__intervals):
+            current_set = Fuzzy_set()
 
-        first_set = Fuzzy_set()
-        for idx, interval in enumerate(self.__intervals):
-            if idx == 1:
-                first_set.add(Member(interval, 1))
-            elif idx == 2:
-                first_set.add(Member(interval, 0.5))
+            if set_idx == 0:
+                for member_idx, interval in enumerate(self.__intervals):
+                    if member_idx == 0:
+                        current_set.add(Member(interval, 1))
+                    elif member_idx == 1:
+                        current_set.add(Member(interval, 0.5))
+                    else:
+                        current_set.add(Member(interval, 0.0))
+
+            elif set_idx == len(self.__intervals) -1:
+                for member_idx, interval in enumerate(self.__intervals):
+                    if member_idx == len(self.__intervals) -1:
+                        current_set.add(Member(interval, 1))
+                    elif member_idx == len(self.__intervals) -2:
+                        current_set.add(Member(interval, 0.5))
+                    else:
+                        current_set.add(Member(interval, 0.0))
+
             else:
-                first_set.add(Member(interval, 0.0))
+                for member_idx, membership_counter in enumerate(self.__intervals):
+                    if member_idx == set_idx:
+                        current_set.add(Member(interval, 1))
+                    elif member_idx == set_idx -1 or member_idx == set_idx + 1:
+                        current_set.add(Member(interval, 0.5))
+                    else:
+                        current_set.add(Member(interval, 0.0))
+            fuzzy_sets.append(current_set)
 
-        for set_idx, set_counter in enumerate(self.__intervals[1:-1]):
-            for member_idx, membership_counter in enumerate(self.__intervals):
-                pass
-
+        return fuzzy_sets
