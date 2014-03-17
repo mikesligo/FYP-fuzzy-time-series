0b08b2591e046939233de2e08d97f4971db87cbc
Mike Gallagher <mikesligo@gmail.com>
2014-03-17 18:10:52 +0000

    getting rmse

--- forecaster.py
+++ forecaster.py
@@ -1,10 +1,20 @@
 import sys
 from olhc_tick import Olhc_Tick
+from math import sqrt
 
 class Forecaster(object):
 
     def evaluate_model(self, fts, eval_file_loc):
+        forecast_val = None
+        error_sq = 0
+
         with open (eval_file_loc, 'r') as read_file:
-            for line in read_file:
+            for idx, line in enumerate(read_file):
                 tick = Olhc_Tick(line)
-                fts.forecast(tick.Close)
+                if forecast_val is not None:
+                    error_sq = error_sq + (forecast_val - tick.Close)**2
+                forecast_val = fts.forecast(tick.Close)
+
+        mse = error_sq/idx
+        rmse = sqrt(mse)
+        return rmse
